c3d9e5d73a7749381bbaa29de48f2272832e9ebd
Winston Venderbush <winstonvenderbush@gmail.com>
2017-04-21 11:16:08 -0400

    pushing files

---
+++ main.py
@@ -0,0 +1,20 @@
+from display import *
+from draw import *
+from parser import *
+from matrix import *
+import math
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+mycolor = [255, 255, 0]
+edges = []
+transform = new_matrix()
+
+# print_matrix( make_bezier() )
+# print
+# print_matrix( make_hermite() )
+# print
+
+#parse_file( 'script', edges, transform, screen, color )
+
+parse_file( 'myscript', edges, transform, screen, mycolor )
